ea5122bc03d3b37628e708bc8d60f42165d68ab3
Fabian Junkert <fajunk001@gmail.com>
2026-05-21 00:08:53 +0200

    chore(nvim): cleanup plugins

--- nvim/dot-config/nvim/lua/plugins/diffview.lua
+++
@@ -1,6 +0,0 @@
-vim.pack.add({
-	'https://github.com/sindrets/diffview.nvim',
-})
-
-require("diffview").setup()
-
--- nvim/dot-config/nvim/lua/plugins/neogit.lua
+++
@@ -1,5 +0,0 @@
-vim.pack.add({
-	'https://github.com/neogitorg/neogit',
-})
-
-require('neogit').setup()
