97334ed47da74e3cfe607915db8b5b17812d41f0
Fabian Junkert <fajunk001@gmail.com>
2026-05-21 18:00:18 +0200

    fix(nvim): remove jk-esc mapping to remove v-mode latency

--- nvim/dot-config/nvim/lua/config/mappings.lua
+++ nvim/dot-config/nvim/lua/config/mappings.lua
@@ -9,7 +9,3 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
--- Map jk to Esc
-vim.keymap.set('i', 'jk', '<Esc>', { desc = 'Map jk to Esc in insert mode' })
-vim.keymap.set('v', 'jk', '<Esc>', { desc = 'Map jk to Esc in visual mode' })
-
