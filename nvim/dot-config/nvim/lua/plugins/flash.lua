9818745dc1d73cba24c206e97dfb25b991486cb8
Fabian Junkert <fajunk001@gmail.com>
2026-05-25 11:08:11 +0200

    refactor(nvim): adapt flash config

--- nvim/dot-config/nvim/lua/plugins/flash.lua
+++ nvim/dot-config/nvim/lua/plugins/flash.lua
@@ -2,4 +2,13 @@ vim.pack.add({
 	'https://github.com/folke/flash.nvim',
 })
 
-vim.keymap.set('n', 's', '<cmd>lua require("flash").jump()<cr>', { desc = 'Jump' })
+require('flash').setup({
+    modes = {
+    -- use flash for default search with backdrop
+    search = {
+      enabled = true,
+      highlight = { backdrop = true },
+  }}
+})
+
+vim.keymap.set('n', '<leader>j', '<cmd>lua require("flash").jump()<cr>', { desc = '[J]ump to match' })
