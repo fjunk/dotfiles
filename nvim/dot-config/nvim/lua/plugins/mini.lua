bb3dd29b4d41517abe4dfd868857ffc950f4b362
Fabian Junkert <fajunk001@gmail.com>
2026-05-29 14:31:07 +0200

    fix(nvim): typo in mini.starter

--- nvim/dot-config/nvim/lua/plugins/mini.lua
+++ nvim/dot-config/nvim/lua/plugins/mini.lua
@@ -1,27 +1,28 @@
-vim.pack.add({ 'https://github.com/nvim-mini/mini.nvim' })
+vim.pack.add({ "https://github.com/nvim-mini/mini.nvim" })
 
 -- mini.ai
-require('mini.ai').setup()
+require("mini.ai").setup()
 
 -- mini.icons
-require('mini.icons').setup()
+require("mini.icons").setup()
 
 -- mini.cmdline
-require('mini.cmdline').setup()
+require("mini.cmdline").setup()
 
 -- mini.ai
-require('mini.surround').setup()
+require("mini.surround").setup()
 
 -- mini.starter
-local starter = require('mini.starter')
+local starter = require("mini.starter")
 
 local my_items = {
-	starter.sections.builtin_actions(), { name = 'File Brower', action = ':Pick files', section = 'Finders' },
+	starter.sections.builtin_actions(),
+	{ name = "File Browser", action = ":Pick files", section = "Finders" },
 }
 
 starter.setup({
 	footer = "",
-    header = "",
+	header = "",
 	items = my_items,
 	evaluate_single = true,
 
@@ -30,84 +31,93 @@ starter.setup({
 	-- |mini.starter| for more details).
 	content_hooks = {
 		starter.gen_hook.adding_bullet(),
-		starter.gen_hook.aligning('center', 'center'),
-		starter.gen_hook.indexing('all', { 'Builtin actions', 'Finders' }),
+		starter.gen_hook.aligning("center", "center"),
+		starter.gen_hook.indexing("all", { "Builtin actions", "Finders" }),
 	},
 })
 
 -- mini.pick
-local pick = require('mini.pick')
+local pick = require("mini.pick")
 
 local centered_picker = function()
-  local height = math.floor(0.618 * vim.o.lines)
-  local width = math.floor(0.618 * vim.o.columns)
-  return {
-    anchor = 'NW', height = height, width = width,
-    row = math.floor(0.5 * (vim.o.lines - height)),
-    col = math.floor(0.5 * (vim.o.columns - width)),
-  }
+	local height = math.floor(0.618 * vim.o.lines)
+	local width = math.floor(0.618 * vim.o.columns)
+	return {
+		anchor = "NW",
+		height = height,
+		width = width,
+		row = math.floor(0.5 * (vim.o.lines - height)),
+		col = math.floor(0.5 * (vim.o.columns - width)),
+	}
 end
 
-pick.setup( { window = { config = centered_picker } })
+pick.setup({ window = { config = centered_picker } })
 
 -- custom dotiles picker
 dotfile_picker = function()
-    pick.builtin.files(nil, {source = { name = 'Dotfiles', cwd = '~/dotfiles', tool = 'rg'}})
+	pick.builtin.files(nil, { source = { name = "Dotfiles", cwd = "~/dotfiles", tool = "rg" } })
 end
 
-vim.keymap.set("n", "<leader>sd", dotfile_picker, { desc = '[S]earch [D]otfiles' })
-vim.keymap.set("n", "<leader>sh", function() pick.builtin.help() end, { desc = '[S]earch [H]elp' })
-vim.keymap.set("n", "<leader>sf", function() pick.builtin.files() end, { desc = '[S]earch [F]ile' }) 
-vim.keymap.set("n", "<leader>sr", function() pick.builtin.resume() end, { desc = '[S]earch [R]esume' })
-vim.keymap.set("n", "<leader><space>", function() pick.builtin.buffers() end, { desc = '[S]earch Buffer' })
+vim.keymap.set("n", "<leader>sd", dotfile_picker, { desc = "[S]earch [D]otfiles" })
+vim.keymap.set("n", "<leader>sh", function()
+	pick.builtin.help()
+end, { desc = "[S]earch [H]elp" })
+vim.keymap.set("n", "<leader>sf", function()
+	pick.builtin.files()
+end, { desc = "[S]earch [F]ile" })
+vim.keymap.set("n", "<leader>sr", function()
+	pick.builtin.resume()
+end, { desc = "[S]earch [R]esume" })
+vim.keymap.set("n", "<leader><space>", function()
+	pick.builtin.buffers()
+end, { desc = "[S]earch Buffer" })
 
 -- mini.extra pickers
-local extra = require('mini.extra')
+local extra = require("mini.extra")
 extra.setup()
 
-vim.keymap.set("n", "<leader>sk", function() extra.pickers.keymaps() end, { desc = '[S]earch [K]eymap' })
-vim.keymap.set("n", "<leader>ss", function() extra.pickers.spellsuggest() end, { desc = '[S]uggest [S]pelling' })
-
-
+vim.keymap.set("n", "<leader>sk", function()
+	extra.pickers.keymaps()
+end, { desc = "[S]earch [K]eymap" })
+vim.keymap.set("n", "<leader>ss", function()
+	extra.pickers.spellsuggest()
+end, { desc = "[S]uggest [S]pelling" })
 
 -- mini.clue
-local miniclue = require('mini.clue')
+local miniclue = require("mini.clue")
 miniclue.setup({
-  triggers = {
-    { mode = { 'n', 'x' }, keys = '<Leader>' },
+	triggers = {
+		{ mode = { "n", "x" }, keys = "<Leader>" },
 
-    { mode = 'n', keys = '[' },
-    { mode = 'n', keys = ']' },
+		{ mode = "n", keys = "[" },
+		{ mode = "n", keys = "]" },
 
-    -- `g` key
-    { mode = { 'n', 'x' }, keys = 'g' },
+		-- `g` key
+		{ mode = { "n", "x" }, keys = "g" },
 
-    -- Marks
-    { mode = { 'n', 'x' }, keys = "'" },
-    { mode = { 'n', 'x' }, keys = '`' },
+		-- Marks
+		{ mode = { "n", "x" }, keys = "'" },
+		{ mode = { "n", "x" }, keys = "`" },
 
-    -- Registers
-    { mode = { 'n', 'x' }, keys = '"' },
-    { mode = { 'i', 'c' }, keys = '<C-r>' },
+		-- Registers
+		{ mode = { "n", "x" }, keys = '"' },
+		{ mode = { "i", "c" }, keys = "<C-r>" },
 
+		-- Window commands
+		{ mode = "n", keys = "<C-w>" },
 
-    -- Window commands
-    { mode = 'n', keys = '<C-w>' },
+		-- `z` key
+		{ mode = { "n", "x" }, keys = "z" },
+	},
 
-    -- `z` key
-    { mode = { 'n', 'x' }, keys = 'z' },
-  },
-
-  clues = {
-    -- Enhance this by adding descriptions for <Leader> mapping groups
-    miniclue.gen_clues.square_brackets(),
-    miniclue.gen_clues.g(),
-    miniclue.gen_clues.marks(),
-    miniclue.gen_clues.registers(),
-    miniclue.gen_clues.windows(),
-    miniclue.gen_clues.z(),
-  },
-  window = { delay = 500, }
+	clues = {
+		-- Enhance this by adding descriptions for <Leader> mapping groups
+		miniclue.gen_clues.square_brackets(),
+		miniclue.gen_clues.g(),
+		miniclue.gen_clues.marks(),
+		miniclue.gen_clues.registers(),
+		miniclue.gen_clues.windows(),
+		miniclue.gen_clues.z(),
+	},
+	window = { delay = 500 },
 })
-
-
