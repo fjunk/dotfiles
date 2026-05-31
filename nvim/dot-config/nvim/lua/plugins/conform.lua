72ad47c0c6855016c406ddd7b65e3686734ba575
Fabian Junkert <fajunk001@gmail.com>
2026-05-31 22:58:38 +0200

    refactor(nvim): add shfmt config for 2 spaces indent

--- nvim/dot-config/nvim/lua/plugins/conform.lua
+++ nvim/dot-config/nvim/lua/plugins/conform.lua
@@ -1,9 +1,15 @@
 vim.pack.add({ "https://github.com/stevearc/conform.nvim" })
 
 require("conform").setup({
+	formatters = {
+		shfmt = {
+			prepend_args = { "-i", "2" },
+		},
+	},
 	formatters_by_ft = {
 		lua = { "stylua" },
 		kotlin = { "ktlint" },
+		sh = { "shfmt" },
 	},
 	format_on_save = {
 		timeout_ms = 3000,
