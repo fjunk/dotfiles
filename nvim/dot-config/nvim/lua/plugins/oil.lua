ce101d570e826db578c5bb5a103ace53cc8c1683
Fabian Junkert <fajunk001@gmail.com>
2026-05-31 16:51:43 +0200

    refactor(nvim): adapt oil config

--- nvim/dot-config/nvim/lua/plugins/oil.lua
+++ nvim/dot-config/nvim/lua/plugins/oil.lua
@@ -2,4 +2,13 @@ vim.pack.add({
 	"https://github.com/stevearc/oil.nvim",
 })
 
-require("oil").setup()
+local oil = require("oil")
+
+oil.setup({
+	view_options = { show_hidden = true },
+	keymaps = { ["q"] = "actions.close" },
+})
+
+vim.keymap.set("n", "<leader>e", function()
+	oil.open()
+end, { desc = "[E]xplore current directory" })
