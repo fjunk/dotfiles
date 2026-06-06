2bf2236f2700631a6aae5912b39b7d3e63c30f52
Fabian Junkert <fajunk001@gmail.com>
2026-06-07 00:58:21 +0200

    feat(nvim): do not use flash for normal search

--- nvim/dot-config/nvim/lua/plugins/flash.lua
+++ nvim/dot-config/nvim/lua/plugins/flash.lua
@@ -2,14 +2,6 @@ vim.pack.add({
 	"https://github.com/folke/flash.nvim",
 })
 
-require("flash").setup({
-	modes = {
-		-- use flash for default search with backdrop
-		search = {
-			enabled = true,
-			highlight = { backdrop = true },
-		},
-	},
-})
+require("flash").setup()
 
 vim.keymap.set("n", "<leader>j", '<cmd>lua require("flash").jump()<cr>', { desc = "[J]ump to match" })
